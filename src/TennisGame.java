8c65cf2c406a62b6f5c7db13d091e38ca0fbff6b
sgrundst <sgrundst@STUPID-PC416.stnet.local>
2016-08-31 16:33:31 +0300

    something

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,4 +1,13 @@
 
 public class TennisGame {
-
+	public static void main(String[] args) {
+	System.out.println("hello");	
+	}
+	
+	public void PlayerScored() {
+		
+	}
+	public void PlayerScored(int i) {
+		
+	}
 }
